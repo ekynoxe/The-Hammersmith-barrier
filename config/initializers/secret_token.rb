05fb97b3b6305257a098cb4550535a72fdbe63ec
Mathieu Davy <matt@ekynoxe.com>
2012-03-22 07:41:54 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hambarrier::Application.config.secret_token = 'd6a2c68014910aff4f1d701f666021e31e99ce48f24f20a2d144f8bce604a34c08dc763d80fb374fb5aaf25e2ee00de5143e7cd314307dd31540600f30534b60'
