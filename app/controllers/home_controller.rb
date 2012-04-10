b7827ec72f205215680c8813efe33c48b6fd763b
Mathieu Davy <matt@ekynoxe.com>
2012-04-10 23:12:00 +0100

    Fixed last status selection order Fixed statuses selection order Added time of latest status at top of home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     def index
-    	@today_status_north = Status.find(:first, :conditions => ["DATE(date) = DATE(?) and location = ?", Time.now, "north"], :limit => 1)
-        @today_status_south = Status.find(:first, :conditions => ["DATE(date) = DATE(?) and location = ?", Time.now, "south"], :limit => 1)
+    	@today_status_north = Status.find(:last, :conditions => ["DATE(date) = DATE(?) and location = ?", Time.now, "north"])
+        @today_status_south = Status.find(:last, :conditions => ["DATE(date) = DATE(?) and location = ?", Time.now, "south"])
 
         @statuses_north = getStatuses(7, 'north')
         @statuses_south = getStatuses(7, 'south')
