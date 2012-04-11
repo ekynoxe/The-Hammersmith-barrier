12cea9f1e95e5daec29a591ac96e6c4f38953baa
Mathieu Davy <matt@ekynoxe.com>
2012-04-11 10:12:00 +0100

    Adding Timezone configuration

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Hambarrier
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.time_zone = 'Europe/London'
   end
 end
