fbaf132916c327802636141a0addbb359e34c5e5
Mathieu Davy <matt@ekynoxe.com>
2012-05-16 16:43:06 +0100

    Updating admin pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def logged_in?
 		not request.authorization.nil?
 	end
+
+    def location_to_char loc
+        ('<b>' + ("north" == loc ? "&#8607; N" : "&#8609; S") + '</b>').html_safe
+    end
 end
